d1d41f933253347f0a8816ffab7fe37f214a9272
Abhradip2004 <deyabhradip594@gmail.com>
2026-01-18 10:56:41 +0530

    ngrok

--- src/lexica/api/main.py
+++ src/lexica/api/main.py
@@ -10,11 +10,12 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.cors_origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
 
     app.include_router(router)
     return app
